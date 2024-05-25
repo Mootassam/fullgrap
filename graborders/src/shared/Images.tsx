be94698f62287b81abe8ec0c40609de6c4830352
Mootassam <Mootassame@gmail.com>
2024-05-25 19:12:34 +0100

    add the new images in the grap

--- graborders/src/shared/Images.tsx
+++ graborders/src/shared/Images.tsx
@@ -1,6 +1,11 @@
-class Images {
+import imagesdata from "src/shared/data/images";
 
-    static  randomImages=()=> { 
+export default class Images {
+  static randomImages = () => {
+    // Pick a random image from the imagesdata array
+    const randomIndex = Math.floor(Math.random() * imagesdata.length);
+    const src = imagesdata[randomIndex].url;
 
-    }
+    return src;
+  };
 }
---
+++ graborders/src/shared/data/images.tsx
@@ -0,0 +1,25 @@
+const imagesdata = [
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfUMVMqVJw2YNtEKM3Wt-bZxs7sBiz0xMZiQ&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQucfeb4lzxk7TdQQg_vY1Gzgh9ySO2vVkqaQ&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQucfeb4lzxk7TdQQg_vY1Gzgh9ySO2vVkqaQ&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWp6jYaP71CWbS_4GYQhPoGPEQ5ceeVWVIHA&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHrYIkmYK8qsrdPMhYpInT_XNcTBcXTp2EOg&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRterG707j3s5TCme2DaKEWzuAU1sr678oZGg&s",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQucfeb4lzxk7TdQQg_vY1Gzgh9ySO2vVkqaQ&s",
+  },
+];
+
+export default imagesdata;
