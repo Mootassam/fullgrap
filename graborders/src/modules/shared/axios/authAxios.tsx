e0e2b87d697220f3f347447f7c89baec4e7a5589
Mootassam <Mootassame@gmail.com>
2024-05-27 14:08:47 +0100

    add the url

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -2,10 +2,11 @@ import axios from "axios";
 import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
-  baseURL: "http://139.162.155.71:8080/api", 
+
    // baseURL: "http://localhost:8080/api", 
   // baseURL: "http://192.168.1.43:8080/api", 
  // baseURL: "http://localhost:8080/api",
+ baseURL: "http://139.162.155.71:8080/api", 
 });
 
 authAxios.interceptors.request.use(async function (options) {
