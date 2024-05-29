302f9639a3c8846cb2f16d82d99f74ef5b2ef6c8
Mootassam <Mootassame@gmail.com>
2024-05-29 17:02:30 +0100

    add the graP

--- server/src/api/record/check.ts
+++ server/src/api/record/check.ts
@@ -5,7 +5,7 @@ import RecordServices from "../../services/recordServices";
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(Permissions.values.categoryRead);
+    // new PermissionChecker(req).validateHas(Permissions.values.categoryRead);
 
     const payload = await new RecordServices(req).check();
 
