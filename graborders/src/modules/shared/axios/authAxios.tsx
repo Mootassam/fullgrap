33c57cf9c56118a27d30b414c7e5ec45cfaa27d5
Mootassam <Mootassame@gmail.com>
2024-08-09 07:06:41 +0100

    add the new commits

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -7,7 +7,7 @@ const authAxios = axios.create({
   // baseURL: "http://localhost:8080/api",
 
   // louis Link
-  baseURL: "http://139.162.161.47:8080/api", 
+  baseURL: "https://eclick-digital.com/api", 
 
   //kiwi LInk
   // baseURL: "http://194.233.175.91:8080/api",
